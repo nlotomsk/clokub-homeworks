595049578509749cea4571dbf5c5e5fb4e69973d
Slava <nlo_tomsk@mail.ru>
2023-11-28 23:46:30 +0700

    work

---
+++ 15.1/test/variables.tf
@@ -0,0 +1,39 @@
+variable "yandex_cloud_id" {
+  default = "b1g4dkqda3n9tf715ll4"
+}
+
+variable "yandex_folder_id" {
+  default = "b1ghbqrg7muqomhugc6i"
+}
+
+variable "a-zone" {
+  default = "ru-central1-a"
+}
+
+variable "nat-instance-image-id" {
+  default = "fd80mrhj8fl2oe87o4e1"
+}
+
+variable "nat-instance-ip" {
+  default = "192.168.10.254"
+}
+
+variable "ubuntu-2004-lts" {
+  default = "fd8nn6vae2u4vo18bv3s"
+}
+
+variable "domain" {
+  default = "netology"
+}
+
+variable "nat-instance-name" {
+  default = "nat-instance-vm1"
+}
+
+variable "public-vm-name" {
+  default = "public-vm1"
+}
+
+variable "private-vm-name" {
+  default = "private-vm1"
+}
